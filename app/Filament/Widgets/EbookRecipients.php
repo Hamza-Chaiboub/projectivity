1f08d462e123fc5ed97be3e08eec4a3f14162c98
Hamza-Chaiboub <hamzachb93@gmail.com>
2026-01-05 17:04:22 -0500

    getting data from db to back office widgets and chart

---
+++ app/Filament/Widgets/EbookRecipients.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Recipient;
+use Carbon\Carbon;
+use Filament\Widgets\ChartWidget;
+use Filament\Widgets\StatsOverviewWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+
+class EbookRecipients extends StatsOverviewWidget
+{
+    protected static ?int $sort = 1;
+    protected function getStats(): array
+    {
+        $all_recipients = Recipient::count();
+        $last_seven_days = Carbon::now()->subDays(7);
+        $recipients_this_week = Recipient::where('created_at', '>=', $last_seven_days)->count();
+        return [
+            Stat::make('Recipients', $all_recipients)
+                ->description('Ebook recipients')
+                ->descriptionIcon('heroicon-m-arrow-trending-up'),
+            Stat::make('For 7 days', $recipients_this_week)
+                ->description($last_seven_days->format('d-m-Y') . ' - ' . Carbon::now()->format('d-m-Y'))
+                ->chart([7, 2, 10, 3, 15, 4, 17]),
+            Stat::make('Average time on page', '3:12'),
+        ];
+    }
+}
